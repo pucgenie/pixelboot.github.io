07fdfafbaf4094eea84b4f7b0277809b535b4c08
Aykut Yilmaz <aykut_78@web.de>
2017-02-02 18:36:36 +0100

    Fix bug

--- assets/scroll.js
+++ assets/scroll.js
@@ -283,7 +283,7 @@ $(".navbar-fixed-top").autoHidingNavbar();
   $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+            scrollTop: ($($anchor.attr('href')).offset().top)
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
