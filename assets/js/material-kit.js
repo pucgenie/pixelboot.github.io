e34394be8719dc488d6dee8a7dc5666cb0b3af6e
Aykut Yilmaz <aykut_78@web.de>
2018-03-30 22:07:47 +0200

    Update material-kit to v2.0.2

---
+++ assets/js/material-kit.js
@@ -0,0 +1,154 @@
+$(window).on("scroll load resize", function(){
+
+  if($(window).scrollTop() > 0) {
+    $('.navbar').addClass("navbar-shadow");
+  }
+  else {
+    $('.navbar').removeClass("navbar-shadow");
+  }
+
+});
+
+$(document).ready(function () {
+
+	$(function(){
+	 function stripTrailingSlash(str) {
+		 if(str.substr(-1) == '/') {
+			 return str.substr(0, str.length - 1);
+		 }
+		 return str;
+	 }
+
+	 var url = window.location.pathname;
+	 var activePage = stripTrailingSlash(url);
+
+	 $('a.nav-link').each(function(){
+		 var currentPage = stripTrailingSlash($(this).attr('href'));
+
+		 if (activePage == currentPage) {
+			 $(this).parent().addClass('active');
+		 }
+	 });
+	 $('a.dropdown-item').each(function(){
+		 var currentPage = stripTrailingSlash($(this).attr('href'));
+
+		 if (activePage == currentPage) {
+			 $(this).addClass('active');
+		 }
+	 });
+ });
+
+  var c, currentScrollTop = 0,
+  navbar = $('.navbar');
+
+  $(window).scroll(function () {
+    var a = $(window).scrollTop();
+    var b = navbar.height();
+
+    currentScrollTop = a;
+
+    if (c < currentScrollTop && a > b + b && !$("html").hasClass("nav-open")) {
+      navbar.addClass("scrollUp");
+    } else if (c > currentScrollTop && !(a <= b)) {
+      navbar.removeClass("scrollUp");
+    }
+    c = currentScrollTop;
+  });
+
+});
+
+$(document.links).filter(function() {
+  return this.hostname != window.location.hostname;
+}).attr('target', '_blank');
+
+
+/*! =========================================================
+ *
+ * Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition)
+ *
+ * =========================================================
+ *
+ * Product Page: https://www.creative-tim.com/product/material-kit-pro
+ * Available with purchase of license from http://www.creative-tim.com/product/material-kit-pro
+ * Copyright 2017 Creative Tim (https://www.creative-tim.com)
+ * License Creative Tim (https://www.creative-tim.com/license)
+ *
+ * ========================================================= */
+
+
+$(document).ready(function() {
+
+    // Init Material scripts for buttons ripples, inputs animations etc, more info on the next link https://github.com/FezVrasta/bootstrap-material-design#materialjs
+    $('body').bootstrapMaterialDesign();
+    window_width = $(window).width();
+    $navbar_collapse = $('.navbar').find('.navbar-collapse');
+
+});
+
+$(document).on('click', '.navbar-toggler', function() {
+    $toggle = $(this);
+
+    if (materialKit.misc.navbar_menu_visible == 1) {
+        $('html').removeClass('nav-open');
+        materialKit.misc.navbar_menu_visible = 0;
+        $('#bodyClick').remove();
+        setTimeout(function() {
+            $toggle.removeClass('toggled');
+        }, 550);
+
+        $('html').removeClass('nav-open-absolute');
+    } else {
+        setTimeout(function() {
+            $toggle.addClass('toggled');
+        }, 580);
+
+
+        div = '<div id="bodyClick"></div>';
+        $(div).appendTo("body").click(function() {
+            $('html').removeClass('nav-open');
+
+            if ($('nav').hasClass('navbar-absolute')) {
+                $('html').removeClass('nav-open-absolute');
+            }
+            materialKit.misc.navbar_menu_visible = 0;
+            $('#bodyClick').remove();
+            setTimeout(function() {
+                $toggle.removeClass('toggled');
+            }, 550);
+        });
+
+        if ($('nav').hasClass('navbar-absolute')) {
+            $('html').addClass('nav-open-absolute');
+        }
+
+        $('html').addClass('nav-open');
+        materialKit.misc.navbar_menu_visible = 1;
+    }
+});
+
+materialKit = {
+  misc: {
+    navbar_menu_visible: 0,
+    window_width: 0,
+    transparent: false,
+    colored_shadows: true,
+    fixedTop: true,
+    navbar_initialized: false,
+    isWindow: document.documentMode || /Edge/.test(navigator.userAgent)
+  },
+
+};
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this,
+            args = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        }, wait);
+        if (immediate && !timeout) func.apply(context, args);
+    };
+}
